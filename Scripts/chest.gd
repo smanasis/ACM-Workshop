03aedf58a9673c32e2c7d5ddd331775587303be5
smanasis <solonmanasis@gmail.com>
2026-03-13 16:01:21 +0200

    delete the addson

--- Scripts/chest.gd
+++ Scripts/chest.gd
@@ -12,6 +12,6 @@ func _process(_delta: float) -> void:
 	pass
 
 #premade signal on the area2d node
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	print("Entered")
 	emit_signal("push_player") #we emit our own signal
